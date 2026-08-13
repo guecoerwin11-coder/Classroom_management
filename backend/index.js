1ea69a19f681964546315547b640d96de844f001
guecoerwin11-coder <guecoewin11@gmail.com>
2026-08-13 08:40:48 +0800

    feat: Add class management routes and integration
    Introduces new API endpoints for managing subjects, sections, and student enrollment. This includes creating a dedicated router file (`classRoute.js`) and integrating it into the main Express application. Routes are protected and utilize the `isTeacher` middleware for certain actions.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const authRouter = require('../backend/src/routes/authRoute')
+const classRouter = require('../backend/src/routes/classRoute')
 const mongooseDatabase = require('../backend/src/configs/mongoDb')
 const app = express()
 
@@ -10,6 +11,7 @@ mongooseDatabase()
 
 app.use(express.json())
 app.use('/api', authRouter)
+app.use('/api', classRouter)
 
 const PORT = process.env.PORT || 3000
 
---
+++ backend/src/routes/classRoute.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const classRouter = express.Router()
+const protect = require('../middleware/protect')
+const isTeacher = require('../middleware/role')
+const enrollStudent = require('../controllers/enrollController')
+const {
+    addSection, getSections, getSection
+} = require('../controllers/sectionController')
+const {
+    createSubject, getSubject
+} = require('../controllers/subjectController')
+
+
+//subject
+classRouter.post('/subjects/add', protect, isTeacher, createSubject)
+classRouter.get('/subjects',protect, getSubject)
+
+//section
+classRouter.post('/sections/add',protect, isTeacher, addSection)
+classRouter.get('/sections',protect, getSections)
+classRouter.get('/sections/:id',protect, getSection)
+
+//enroll Student
+classRouter.post('/enroll',protect, isTeacher, enrollStudent)
+
+module.exports = classRouter
